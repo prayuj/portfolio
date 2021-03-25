b6b9dd7841a0aecf4b5fbeef10814ca0672bb4e1
AzureAD\PrayujPillai <prayuj.pillai@pens.com>
2021-03-25 18:33:24 +0530

    Slide changes (Really have got to putting content rather than worry about css)

--- src/components/mainExperience.js
+++ src/components/mainExperience.js
@@ -1,20 +1,17 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Table from 'react-bootstrap/Table';
-
 const Experience = () => {
     return (
         <>
+            <div className="experience-heading display-flex justify-content-center align-items-center"><h1>My Experience</h1></div>
             <div className="full-page-slide" style={{ width: '100%', height: '100vh' }}>
-                <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}><h1>Slide 1.1</h1></div>
+                <div className="display-flex justify-content-center align-items-center" ><h1>Cimpress</h1></div>
             </div>
             <div className="full-page-slide" style={{ width: '100%', height: '100vh' }}>
-                <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}><h1>Slide 1.2</h1></div>
+                <div className="display-flex justify-content-center align-items-center"><h1>Aegis School of Data Science</h1></div>
             </div>
             <div className="full-page-slide" style={{ width: '100%', height: '100vh' }}>
-                <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}><h1>Slide 1.3</h1></div>
+                <div className="display-flex justify-content-center align-items-center"><h1>CaratLane</h1></div>
             </div>
+
         </>);
 }
 
