530c8f3b570d2bb8b61a3bed35f9770cb355070e
AzureAD\PrayujPillai <prayuj.pillai@pens.com>
2021-03-28 18:24:54 +0530

    Added some content

--- src/components/mainExperience.js
+++ src/components/mainExperience.js
@@ -2,21 +2,43 @@ const Experience = () => {
     return (
         <>
             <div className="experience-heading display-flex justify-content-center align-items-center"><h1>My Experience</h1></div>
-            <div className="full-page-slide container" style={{ width: '100%', height: '100vh' }}>
-                <div className="display-flex justify-content-center align-items-center flex-direction-column" >
-                    <h1>Cimpress</h1>
-                    <h2>August 2020 - Present</h2>
-                    <p>Worked in Cimpress’ subsidiary National Pen which is a multi-lingual E-commerce platform
-                    operating in 20+ nations. Developed Lambdas and API’s in Node.js for efficiently improving
-                    website performance and used Magnolia as a Content Management System (CMS). Had hands-on
+            <div className="full-page-slide" style={{ width: '100%', height: '100vh' }}>
+                <div className="display-flex justify-content-center align-items-center container">
+                    <div className="display-flex justify-content-center align-items-center flex-direction-column" >
+                        <h1>Cimpress</h1>
+                        <h2>August 2020 - Present</h2>
+                        <p>Worked in Cimpress’ subsidiary National Pen which is a multi-lingual E-commerce platform
+                        operating in 20+ nations. Completely Reengineered Website's' Navigation. Developed Lambdas and API’s in Node.js for efficiently improving
+                        website performance and used Magnolia as a Content Management System (CMS). Had hands-on
                         experience with variety of technologies such as AWS, Sentry, Serverless, FTL, React.JS, GraphQL. </p>
+                    </div>
                 </div>
+
             </div>
             <div className="full-page-slide" style={{ width: '100%', height: '100vh' }}>
-                <div className="display-flex justify-content-center align-items-center"><h1>Aegis School of Data Science</h1></div>
+                <div className="display-flex justify-content-center align-items-center container">
+                    <div className="display-flex justify-content-center align-items-center flex-direction-column" >
+                        <h1>Aegis School of Data Science</h1>
+                        <h2>June 2019 - August 2019</h2>
+                        <p>Worked on a Job Recommendation Model for Students, based on Job Requirement Skill Set and Grades of Students in the Institute for.
+                        Used Python as a modelling tool.
+                        </p>
+                    </div>
+                </div>
+
             </div>
             <div className="full-page-slide" style={{ width: '100%', height: '100vh' }}>
-                <div className="display-flex justify-content-center align-items-center"><h1>CaratLane</h1></div>
+                <div className="display-flex justify-content-center align-items-center container">
+                    <div className="display-flex justify-content-center align-items-center flex-direction-column" >
+                        <h1>Cimpress</h1>
+                        <h2>August 2020 - Present</h2>
+                        <p>Worked in Cimpress’ subsidiary National Pen which is a multi-lingual E-commerce platform
+                        operating in 20+ nations. Completely Reengineered Website's' Navigation. Developed Lambdas and API’s in Node.js for efficiently improving
+                        website performance and used Magnolia as a Content Management System (CMS). Had hands-on
+                        experience with variety of technologies such as AWS, Sentry, Serverless, FTL, React.JS, GraphQL. </p>
+                    </div>
+                </div>
+
             </div>
 
         </>);
