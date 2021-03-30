d6f52e92511f2f997b6552142143dd61c41a82d3
AzureAD\PrayujPillai <prayuj.pillai@pens.com>
2021-03-30 10:19:41 +0530

    every time I tell myself I'll put content, instead I waste time adjusting css :(

--- src/components/mainExperience.js
+++ src/components/mainExperience.js
@@ -17,7 +17,7 @@ const Experience = () => {
 
             </div>
             <div className="full-page-slide" style={{ width: '100%', height: '100vh' }}>
-                <div className="display-flex justify-content-center align-items-center container-sm">
+                <div className="display-flex justify-content-center align-items-center container">
                     <div className="display-flex justify-content-center align-items-center flex-direction-column" >
                         <h1>Aegis School of Data Science</h1>
                         <h2>Data Science Intern</h2>
