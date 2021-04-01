79ae12f146c37495fc3d21150f444a44feb2de3a
AzureAD\PrayujPillai <prayuj.pillai@pens.com>
2021-04-01 19:55:50 +0530

    Experience change

--- src/components/mainProjects.js
+++ src/components/mainProjects.js
@@ -5,51 +5,46 @@ import CardDeck from 'react-bootstrap/CardDeck'
 import Card from 'react-bootstrap/Card'
 
 const Projects = () => {
-    return (<div className="display-flex justify-content-center align-items-center flex-direction-column">
-        <h1>Projects</h1>
-        <CardDeck>
-            <Card border="primary">
-                <Card.Body>
-                    <div><h3>Task Manager API</h3><a href="https://github.com/prayuj/task-manager"><i class="fas fa-external-link-alt"></i></a></div>
-                </Card.Body>
-                <Card.Footer>
-                    <small className="text-muted">NodeJS</small>
-                </Card.Footer>
-            </Card>
-            <Card>
-                <Card.Body>
-                    <h3>Chat App using Socket.io</h3>
-                    <p>This is some text within a card body.</p>
-                </Card.Body>
-            </Card>
-            <Card>
-                <Card.Body>
-                    <h3>Game Renting Server</h3>
-                    <p>This is some text within a card body.</p>
-                </Card.Body>
-            </Card>
-            <Card>
-                <Card.Body>
-                    <h3>Bill Splitter App</h3>
-                    <p>This is some text within a card body.</p>
-                </Card.Body>
-            </Card>
-            <Card>
-                <Card.Body>
-                    <h3>B.E. Project</h3>
-                    <p>This is some text within a card body.</p>
-                </Card.Body>
-            </Card>
-            <Card>
-                <Card.Body>
-                    <h3>B.E. Project</h3>
-                    <p>This is some text within a card body.</p>
-                </Card.Body>
-            </Card>
-        </CardDeck>
+    const projects = [
+        {
+            name: 'Task Manager API',
+            link: 'https://github.com/prayuj/task-manager',
+            languages: 'NodeJS, Jest'
+        },
+        {
+            name: 'Chat App using Socket.io',
+            link: 'https://github.com/prayuj/task-manager',
+            languages: 'NodeJS, Socket.io'
+        },
+        {
+            name: 'Game Renting Service',
+            link: 'https://github.com/prayuj/task-manager',
+            languages: 'React, Express'
+        },
+        {
+            name: 'Bill Splitter App',
+            link: 'https://github.com/prayuj/task-manager',
+            languages: 'React, Flask'
+        }
+    ]
 
-    </div>
-    );
+    return (
+        <div className="display-flex justify-content-center align-items-center flex-direction-column">
+            <h1>Projects</h1>
+            <CardDeck>
+                {projects.map((project) => (
+                    <Card border="primary">
+                        <Card.Body>
+                            <div><h3>{project.name}</h3><a href={project.link} style={{ width: 'max-content' }} target="_blank"><i class="fas fa-external-link-alt"></i></a></div>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">{project.languages}</small>
+                        </Card.Footer>
+                    </Card>
+                ))}
+            </CardDeck>
+        </div>
+    )
 }
 
 export default Projects;
