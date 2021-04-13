a78f0451e6d2efd8f25b2b3f886ad73aa2d9028f
AzureAD\PrayujPillai <prayuj@gmail.com>
2021-04-13 22:26:15 +0530

    Added about me transition

--- src/components/mainAbout.js
+++ src/components/mainAbout.js
@@ -1,32 +1,52 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { useState, useEffect } from 'react';
 
 const About = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        const timeout = setTimeout(() => setIsMounted(true), 2000);
+        return () => clearTimeout(timeout);
+    }, []);
+
+    const timeout = 2000;
+
+    const one = <h1><span className="underline-style">About me</span></h1>
+    const two = <p>Hello! My name is Prayuj and I am a Software Engineer based in Mumbai and I enjoy Full Stack Development
+    with more empahsis on backend systems (which is pretty evident from the design of this website 😅).</p>
+    const three = <p>I currently work at <a href="https://cimpress.com/">Cimpress</a> as part of their Business subsidiary, <a href="https://www.pens.com/ie/">National Pen.</a></p>
+    const four = <p>Here are a few technologies I am proficient in:</p>
+    const five = <ul className="display-flex">
+        <div className="display-flex flex-direction-column tech-list-items">
+            <li>JavaScript</li>
+            <li>NodeJS</li>
+            <li>ReactJS</li>
+            <li>MongoDB</li>
+        </div>
+        <div className="display-flex flex-direction-column tech-list-items">
+            <li>Flask</li>
+            <li>jQuery</li>
+            <li>Java</li>
+            <li>HTML/CSS</li>
+        </div>
+    </ul>
+
+    const items = [one, two, three, four, five]
+
     return (
         <Container className="h-100">
             <Row className="h-100 justify-content-center align-items-center">
                 <Col className="display-flex flex-direction-column">
-                    <h1><span className="underline-style">About me</span></h1>
-                    <p>Hello! My name is Prayuj and I am a Software Engineer based in Mumbai and I enjoy Full Stack Development
-                        with more empahsis on backend systems (which is pretty evident from the design of this website 😅).</p>
-                    <p>I currently work at <a href="https://cimpress.com/">Cimpress</a> as part of their Business subsidiary, <a href="https://www.pens.com/ie/">National Pen.</a></p>
-                    <p>Here are a few technologies I am proficient in:</p>
-                    <ul className="display-flex">
-                        <div className="display-flex flex-direction-column tech-list-items">
-                            <li>JavaScript</li>
-                            <li>NodeJS</li>
-                            <li>ReactJS</li>
-                            <li>MongoDB</li>
-                        </div>
-                        <div className="display-flex flex-direction-column tech-list-items">
-                            <li>Flask</li>
-                            <li>jQuery</li>
-                            <li>Java</li>
-                            <li>HTML/CSS</li>
-                        </div>
-
-                    </ul>
+                    <TransitionGroup component={null}>
+                        {isMounted &&
+                            items.map((item, i) => (
+                                <CSSTransition key={i} classNames="fadeup" timeout={timeout}>
+                                    <div style={{ transitionDelay: `${i + 1}00ms` }}>{item}</div>
+                                </CSSTransition>
+                            ))}
+                    </TransitionGroup>
                 </Col>
             </Row>
         </Container>
