6ac23ee248815fb9578e078a195297e83ecfc4af
AzureAD\PrayujPillai <prayuj@gmail.com>
2021-04-13 23:12:18 +0530

    updated about us

--- src/components/mainAbout.js
+++ src/components/mainAbout.js
@@ -18,19 +18,15 @@ const About = () => {
     with more empahsis on backend systems (which is pretty evident from the design of this website 😅).</p>
     const three = <p>I currently work at <a href="https://cimpress.com/">Cimpress</a> as part of their Business subsidiary, <a href="https://www.pens.com/ie/">National Pen.</a></p>
     const four = <p>Here are a few technologies I am proficient in:</p>
-    const five = <ul className="display-flex">
-        <div className="display-flex flex-direction-column tech-list-items">
-            <li>JavaScript</li>
-            <li>NodeJS</li>
-            <li>ReactJS</li>
-            <li>MongoDB</li>
-        </div>
-        <div className="display-flex flex-direction-column tech-list-items">
-            <li>Flask</li>
-            <li>jQuery</li>
-            <li>Java</li>
-            <li>HTML/CSS</li>
-        </div>
+    const five = <ul className="technology-list">
+        <li>JavaScript</li>
+        <li>NodeJS</li>
+        <li>ReactJS</li>
+        <li>MongoDB</li>
+        <li>Flask</li>
+        <li>jQuery</li>
+        <li>Java</li>
+        <li>HTML/CSS</li>
     </ul>
 
     const items = [one, two, three, four, five]
