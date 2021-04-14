23f99c535ede4814077a047709f5f288d95528ff
AzureAD\PrayujPillai <prayuj@gmail.com>
2021-04-14 23:48:11 +0530

    Added contact me transitions

--- src/components/mainContact.js
+++ src/components/mainContact.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-class Contact extends Component {
-    render() {
-        return (<Container className="main-container h-100 desktop">
-            <Row className="h-100 justify-content-center align-items-center">
-                <Col className="display-flex justify-content-center align-items-center flex-direction-column">
-                    <h1><span className="underline-style">Connect with me:</span></h1>
-                </Col>
-            </Row>
-        </Container>);
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { useState, useEffect } from 'react';
+const Contact = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        const timeout = setTimeout(() => setIsMounted(true), 2000);
+        return () => clearTimeout(timeout);
+    }, []);
 
-    }
+    const timeout = 2000;
+
+    return (<Container className="main-container h-100 desktop">
+        <Row className="h-100 justify-content-center align-items-center">
+            <TransitionGroup component={null}>
+                {isMounted && <CSSTransition key={0} classNames="fadeup" timeout={timeout}>
+                    <Col className="display-flex justify-content-center align-items-center flex-direction-column">
+                        <h1><span className="underline-style">Connect with me:</span></h1>
+                    </Col></CSSTransition>}
+            </TransitionGroup>
+
+        </Row>
+    </Container>);
 }
 
 export default Contact;
