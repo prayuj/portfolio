5a9536e40d74e4b7a2eaf73956b405fbab0325dc
AzureAD\PrayujPillai <prayuj@gmail.com>
2021-04-16 00:25:57 +0530

    Added transition to header and toggle

--- src/components/dayNightToggler.js
+++ src/components/dayNightToggler.js
@@ -4,25 +4,30 @@ import {
     isMobileOnly,
     isDesktop
 } from "react-device-detect";
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 class DayNightToggler extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isMounted: false
+        }
         const cookieMode = document.cookie.match('(^|;)\\s*mode\\s*=\\s*([^;]+)')?.pop() || ''
         if (cookieMode) {
-            this.state = {
-                isDarkMode: cookieMode === 'dark'
-            }
+            this.state.isDarkMode = cookieMode === 'dark';
             document.getElementById('root').className = cookieMode;
         }
         else {
             document.cookie = "mode=dark"
-            this.state = {
-                isDarkMode: true
-            }
+            this.state.isDarkMode = true;
             document.getElementById('root').className = 'dark'
         }
         this.setIsDarkMode = this.setIsDarkMode.bind(this)
+    }
+
+    componentDidMount() {
+        const timeout = setTimeout(() => this.setState({ isMounted: true }), 1400);
+        return () => clearTimeout(timeout);
     }
 
     setIsDarkMode() {
@@ -37,19 +42,28 @@ class DayNightToggler extends Component {
     render() {
         if (isDesktop)
             return (
-                <DarkModeToggle
-                    onChange={this.setIsDarkMode}
-                    checked={this.state.isDarkMode}
-                    className="day-night-toggle"
-                />
+                <TransitionGroup component={null}>
+                    {this.state.isMounted && <CSSTransition classNames='fadeup' timeout={1000}>
+                        <DarkModeToggle
+                            onChange={this.setIsDarkMode}
+                            checked={this.state.isDarkMode}
+                            className="day-night-toggle"
+                        />
+                    </CSSTransition>}
+                </TransitionGroup>
             );
         else if (isMobileOnly) {
-            return (<DarkModeToggle
-                onChange={this.setIsDarkMode}
-                checked={this.state.isDarkMode}
-                className="day-night-toggle-mobile"
-                size={60}
-            />)
+            return (<TransitionGroup component={null}>
+                {this.state.isMounted && <CSSTransition classNames='fadeup' timeout={1000}>
+                    <DarkModeToggle
+                        onChange={this.setIsDarkMode}
+                        checked={this.state.isDarkMode}
+                        className="day-night-toggle-mobile"
+                        size={60}
+                    />
+                </CSSTransition>}
+            </TransitionGroup>
+            )
         }
         else {
             <></>
--- src/components/portfolioHeading.js
+++ src/components/portfolioHeading.js
@@ -1,10 +1,21 @@
 import {
     isDesktop
 } from "react-device-detect";
+import { useState, useEffect } from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const Heading = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        const timeout = setTimeout(() => setIsMounted(true), 100);
+        return () => clearTimeout(timeout);
+    }, []);
     if (isDesktop) {
-        return (<div className="portfolio-heading display-flex"><span><h2>prayuj</h2></span><span className="accent-style"><h2>.tech</h2></span></div>)
+        return (<TransitionGroup component={null}>
+            {isMounted && <CSSTransition classNames='faderight' timeout={1000}>
+                <div className="portfolio-heading display-flex"><span><h2>prayuj</h2></span><span className="accent-style"><h2>.tech</h2></span></div>
+            </CSSTransition>}
+        </TransitionGroup>)
     }
     return (<></>);
 }
