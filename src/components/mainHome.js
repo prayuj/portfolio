f8403aaad7e8e7595fc7921aabad8ab835cbbc02
AzureAD\PrayujPillai <prayuj@gmail.com>
2021-04-21 00:27:20 +0530

    can someone helppp

--- src/components/mainHome.js
+++ src/components/mainHome.js
@@ -2,8 +2,27 @@ import {
     isMobileOnly,
     isDesktop
 } from "react-device-detect";
-import HomeDesktop from './desktop/home'
+// import HomeDesktop from './desktop/home'
 import HomeMobile from './mobile/home'
+import Loadable from 'react-loadable';
+
+function Loading(props) {
+    if (props.error) {
+        return <div>Error! <button onClick={props.retry}>Retry</button></div>;
+    } else {
+        return <div>Loading...</div>;
+    }
+}
+
+const HomeDesktop = Loadable({
+    loader: () => import('./desktop/home'),
+    loading: Loading,
+    render(loaded, props) {
+        console.log(loaded, props)
+        let Component = loaded.default;
+        return <Component {...props} />;
+    }
+})
 
 const Home = ({ show, isDarkMode }) => {
     if (isDesktop)
